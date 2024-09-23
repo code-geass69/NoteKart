6af285dbb38b23f569b535f1e07fd71be205edea
Atharv More <atharvmore30@gmail.com>
2024-09-23 23:20:00 +0530

    new file:   src/components/AddNote.js 	new file:   src/components/Alert.js 	modified:   src/components/Home.js 	new file:   src/components/Login.js 	modified:   src/components/Navbar.js 	new file:   src/components/Noteitem.js 	new file:   src/components/Notes.js 	new file:   src/components/Signup.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,10 @@
-import React from 'react'
+import Notes from './Notes';
 
-const Home = () => {
+export const Home = () => {
+
     return (
-        <div className='container'>
-            <h2>Add a Note</h2>
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-            </div>
-            <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div>
+        <div> 
+            <Notes/>
         </div>
     )
 }
-
-export default Home
